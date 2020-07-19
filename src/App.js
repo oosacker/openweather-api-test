f48c81973e7000a811b4cae8be0d90aaa0bda89d
Natsuki Hasegawa <oosacker@gmail.com>
2020-07-19 19:16:47 +1200

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import { Container, Row, Col } from 'react-grid-system';
 
-function App() {
+const App = () => {
+  const [posts, setPost] = useState([]);
+
+  async function fetchData() {
+    const res = await fetch("https://api.openweathermap.org/data/2.5/weather?q=wellington&appid=1dd2717b1abf4fe325703be72955b65d&units=metric");
+
+    console.log(res);
+
+    res
+      .json()
+      .then(res => {
+        setPost(res);
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  }
+
+  useEffect(() => {
+    console.log('useEffect');
+    fetchData();
+
+    // fetch('https://api.openweathermap.org/data/2.5/weather?q=wellington&appid=1dd2717b1abf4fe325703be72955b65d')
+    //   .then(res => res.text())
+    //   .then(text => console.log(text))  
+    //   // .then(res => setPost(res))
+    //   .catch(err => console.log(err))
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <ul>{
+          posts.map(post => 
+            <li key={post.id}>
+              {post.title}
+            </li>)
+        }</ul> */}
+      <Container>
+
+        <Row>
+          {posts.name}
+        </Row>
+
+      </Container>
+
     </div>
   );
-}
+};
 
 export default App;
