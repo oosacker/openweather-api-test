812f6cab379964ce8ab1cf6ef217c6a7ec7510cd
Natsuki Hasegawa <oosacker@gmail.com>
2020-07-23 14:49:52 +1200

    add westher icon and sass styling

--- src/App.js
+++ src/App.js
@@ -89,32 +89,31 @@ function App () {
   else {
     return (
 
-      <>
-        <Container>
-          <Row>
-            <form onSubmit={handleSubmit}>
-              <label htmlFor='city_input'>Enter city: </label>
-              <input id='city_input' type='input'  name='city' onChange={handleChange}/>
-              <input type="submit" value="Submit" />
-              {/* <button onClick={handleSubmit}>Submit</button> */}
-            </form>
-          </Row>
+      <Container className={'main-container'}>
+        <Row align={'center'} justify={'center'} className={'row-1'}>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor='city_input'>Enter city: </label>
+            <input id='city_input' type='input'  name='city' onChange={handleChange} size="50"/>
+            <input type="submit" value="Submit" />
+            {/* <button onClick={handleSubmit}>Submit</button> */}
+          </form>
+        </Row>
 
-          <Row>
-            {weatherData && (
-              <div>
+        {weatherData && (
+          <Row align={'center'} justify={'center'} className={'row-2'}>
+              <Container className={'weather-result'}>
                 <h3>{city.toUpperCase()}</h3>
                 <p>Temperature: {weatherData.main.temp} degC</p>
                 <p>Feels like: {weatherData.main.feels_like} degC</p>
                 <p>Humidity: {weatherData.main.humidity} %</p>
-                <p>{weatherData.weather[0].main +' - '+ weatherData.weather[0].description} </p>
-                {/* <p>{weatherData.weather[0].description}</p> */}
-              </div>
-            )}
-
+                <div className='weather-icon-container'>
+                  <img className={'weather-icon'} src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}></img>
+                </div>
+                <p>{weatherData.weather[0].main +': '+ weatherData.weather[0].description} </p>
+              </Container>
           </Row>
-        </Container>
-      </>
+        )}
+      </Container>
     );
   };
 };
